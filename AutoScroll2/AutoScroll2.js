fbbf2223387d5140296e200f045f4221d1de709f
EilenC <chhccht2136@qq.com>
2023-11-02 10:41:43 +0800

    feat: 优化使用提示,处理已知问题
    feat
    1.增加滚动开关提示
    2.edge(Chromium)使用 requestAnimationFrame
    进行滚动
    fix
    1.chrome 缩放比例<100% 失效滚动
    
    备注: 目前chrome缩放<50%后依旧会出现无法滚动

--- AutoScroll2/AutoScroll2.js
+++ AutoScroll2/AutoScroll2.js
@@ -8,33 +8,157 @@
 // @namespace https://greasyfork.org/users/319354
 // ==/UserScript==
 
-;(function(document) {
-    var enable = false;
-    var handler = 0;
-    var time = 200;
-    var step = 1;
-    var keyNum=1;
-    function keyd(e){
-        console.log(e.key);
-        if(e.key == "`"){
-            keyNum++;
-            if(keyNum%2 == 1){
-                enable = !enable;
-                clearTimeout(handler);
-                if (enable) aScroll();
-            }
-        }else if(e.key == "ArrowRight"){
-            time -= 20;
-        }else if(e.key == "ArrowLeft"){
-            time += 20;
+; (function (document) {
+    'use strict';
+
+    function roundToTwoDecimalPlaces(number) {
+        return Number(number.toFixed(2));
+    }
+    // 存储当前通知的定时器
+    let notificationTimer = null;
+    let notification = document.createElement('div');
+    // 显示通知函数
+    function showNotification(message, duration) {
+        // 如果有，清除之前的定时器
+        clearTimeout(notificationTimer);
+
+        // 创建提示元素
+        notification.textContent = message;
+
+        // 样式设置
+        notification.style.position = 'fixed';
+        notification.style.top = '50%';
+        notification.style.left = '50%';
+        notification.style.transform = 'translate(-50%, -50%)';
+        notification.style.background = 'black';
+        notification.style.color = 'white';
+        notification.style.fontWeight = 'bold';
+        notification.style.fontSize = '40px';
+        notification.style.padding = '20px';
+        notification.style.borderRadius = '10px';
+
+        // 将元素添加到页面
+        document.body.appendChild(notification);
+
+        // 设置定时器，在指定的时间后移除通知
+        notificationTimer = setTimeout(function () {
+            document.body.removeChild(notification);
+            notificationTimer = null; // 通知显示完成后清空定时器
+        }, duration);
+    }
+    if (!Date.now) {
+        Date.now = function () { return new Date().getTime(); };
+    }
+    //from https://github.com/darius/requestAnimationFrame/blob/master/requestAnimationFrame.js
+    (function () {
+        'use strict';
+
+        var vendors = ['webkit', 'moz'];
+        for (var i = 0; i < vendors.length && !window.requestAnimationFrame; ++i) {
+            var vp = vendors[i];
+            window.requestAnimationFrame = window[vp + 'RequestAnimationFrame'];
+            window.cancelAnimationFrame = (window[vp + 'CancelAnimationFrame']
+                || window[vp + 'CancelRequestAnimationFrame']);
         }
+        if (/iP(ad|hone|od).*OS 6/.test(window.navigator.userAgent) // iOS6 is buggy
+            || !window.requestAnimationFrame || !window.cancelAnimationFrame) {
+            var lastTime = 0;
+            window.requestAnimationFrame = function (callback) {
+                var now = Date.now();
+                var nextTime = Math.max(lastTime + 16, now);
+                return setTimeout(function () { callback(lastTime = nextTime); },
+                    nextTime - now);
+            };
+            window.cancelAnimationFrame = clearTimeout;
+        }
+    }());
+
+    'use strict';
+
+    let keyDownCount = 0;
+    let lastKeyDownTime = 0;
+    let enable = false;
+    let scrollSpeed = 0.55;
+    let animationFrameId = 0;
+    let tipsTime = 300;
+    let handler = 0;
+    let time = 200;
+    let isEdge = navigator.userAgent.includes('Edg') ? true : false;
+
+    console.log('isEdge:',isEdge);
+
+    function toggleScroll() {
+        enable = !enable;
+        if (enable) {
+            if (isEdge) {
+                edgeAutoScroll();
+            } else {
+                autoScroll();
+            }
+            showNotification('AutoScroll:Open', tipsTime);
+        } else {
+            if (isEdge){
+                cancelAnimationFrame(animationFrameId);
+            }else{
+                clearTimeout(handler);
+            }
+            showNotification('AutoScroll:Close', tipsTime);
+        };
     }
 
-    document.body.removeEventListener('keydown', keyd);
-    document.body.addEventListener('keydown', keyd);
+    function keydownHandler(e) {
+        console.log(e.key);
+        let currentTime = new Date().getTime();
+        switch (e.key) {
+            case "`":
+                if (currentTime - lastKeyDownTime < 300) {
+                    keyDownCount++;
+                } else {
+                    keyDownCount = 1;
+                }
 
-    var aScroll = function() {
-        if (enable) document.documentElement.scrollTop += 1;
-        handler = setTimeout(aScroll, time);
-    };
+                lastKeyDownTime = currentTime;
+
+                if (keyDownCount === 2) {
+                    toggleScroll();
+                    keyDownCount = 0;
+                }
+                break;
+            case "ArrowRight":
+                if (isEdge) {
+                    scrollSpeed = roundToTwoDecimalPlaces(scrollSpeed += 0.02);
+                    showNotification('Speed:' + scrollSpeed, tipsTime);
+                }else{
+                    time -= 20;
+                }
+                break;
+            case "ArrowLeft":
+                if(isEdge) {
+                    if (scrollSpeed - 0.02 > 0) {
+                        scrollSpeed -= 0.02
+                    }
+                    scrollSpeed = roundToTwoDecimalPlaces(scrollSpeed);
+                    showNotification('Speed:' + scrollSpeed, tipsTime);
+                }else{
+                    time += 20;
+                }
+                break;
+        }
+    }
+    function edgeAutoScroll() {
+        console.log("edgeAutoScroll: " + document.documentElement.scrollTop);
+        console.log(window.scrollY, window.pageYOffset, document.documentElement.scrollTop);
+        document.documentElement.scrollTop += scrollSpeed;
+        animationFrameId = requestAnimationFrame(edgeAutoScroll);
+    }
+    function autoScroll() {
+        console.log("autoScroll: " + document.documentElement.scrollTop);
+        console.log(window.scrollY, window.pageYOffset, document.documentElement.scrollTop);
+        document.documentElement.scrollTop += 2;
+        handler = setTimeout(autoScroll, time);
+    }
+
+    // 监听键盘事件
+    document.body.removeEventListener('keydown', keydownHandler);
+    document.body.addEventListener('keydown', keydownHandler);
 })(document);
