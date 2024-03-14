ad9b10b6d199ad9ab61ba13e0c7aa13eb92258ba
EilenC <chhccht2136@qq.com>
2024-03-15 02:43:06 +0800

    refactor: 重写滚动逻辑淘汰使用 `setTimeout` 方式实现滚动避免因delay时间最小为0问题
    1. 重写滚动逻辑淘汰使用 setTimeout 方式实现滚动避免因delay时间最小为0问题
    2. 更新README
    
    refactor:
    rewrite the scrolling logic to eliminate the use of setTimeout to
    implement scrolling to avoid the problem of delay times minimizing to
    0.
    
    1. Rewrite the scrolling logic to use setTimeout to avoid the
    problem of delay time minimizing to zero.
    2. Update the README

--- AutoScroll2/AutoScroll2.js
+++ AutoScroll2/AutoScroll2.js
@@ -1,31 +1,28 @@
 // ==UserScript==
 // @name        AutoScroll2
-// @description AutoScroll - 自动向下滚动页面,支持快捷键
+// @description AutoScroll - Automatically scroll down the page, supporting hotkey control switches and speed adjustment (自动向下滚动页面，支持热键控制开关和速度调节)
 // @include     http*
-// @version     2023/11/02
+// @version     1.0.2
 // @author      Eilen https://github.com/EilenC
 // @grant       none
 // @namespace https://greasyfork.org/users/319354
+// @supportURL https://github.com/EilenC/Tampermonkey-Scripts/blob/master/AutoScroll2/AutoScroll2.js
 // ==/UserScript==
 
-; (function (document) {
+(function (document) {
     'use strict';
 
     function roundToTwoDecimalPlaces(number) {
         return Number(number.toFixed(2));
     }
-    // 存储当前通知的定时器
+
     let notificationTimer = null;
     let notification = document.createElement('div');
-    // 显示通知函数
+    let tipsTime = 300;
+
     function showNotification(message, duration) {
-        // 如果有，清除之前的定时器
         clearTimeout(notificationTimer);
-
-        // 创建提示元素
         notification.textContent = message;
-
-        // 样式设置
         notification.style.position = 'fixed';
         notification.style.top = '50%';
         notification.style.left = '50%';
@@ -36,81 +33,77 @@
         notification.style.fontSize = '40px';
         notification.style.padding = '20px';
         notification.style.borderRadius = '10px';
-
-        // 将元素添加到页面
+        notification.style.zIndex = '9999';
         document.body.appendChild(notification);
-
-        // 设置定时器，在指定的时间后移除通知
         notificationTimer = setTimeout(function () {
             document.body.removeChild(notification);
-            notificationTimer = null; // 通知显示完成后清空定时器
+            notificationTimer = null;
         }, duration);
     }
-    if (!Date.now) {
-        Date.now = function () { return new Date().getTime(); };
-    }
-    //from https://github.com/darius/requestAnimationFrame/blob/master/requestAnimationFrame.js
-    (function () {
-        'use strict';
 
-        var vendors = ['webkit', 'moz'];
-        for (var i = 0; i < vendors.length && !window.requestAnimationFrame; ++i) {
-            var vp = vendors[i];
-            window.requestAnimationFrame = window[vp + 'RequestAnimationFrame'];
-            window.cancelAnimationFrame = (window[vp + 'CancelAnimationFrame']
-                || window[vp + 'CancelRequestAnimationFrame']);
-        }
-        if (/iP(ad|hone|od).*OS 6/.test(window.navigator.userAgent) // iOS6 is buggy
-            || !window.requestAnimationFrame || !window.cancelAnimationFrame) {
-            var lastTime = 0;
-            window.requestAnimationFrame = function (callback) {
-                var now = Date.now();
-                var nextTime = Math.max(lastTime + 16, now);
-                return setTimeout(function () { callback(lastTime = nextTime); },
-                    nextTime - now);
-            };
-            window.cancelAnimationFrame = clearTimeout;
-        }
-    }());
+    let scrollY = 0;
+    let scrollSpeed = 0.5; // 默认滚动速度
+    let isScrolling = false;
 
-    'use strict';
+    let lastScrollTime = performance.now();
+    const targetFrameDelay = 1000 / 60; // 目标帧率为每秒 60 帧
+    let animationFrameId = null;
 
-    let keyDownCount = 0;
-    let lastKeyDownTime = 0;
-    let enable = false;
-    let scrollSpeed = 0.55;
-    let animationFrameId = 0;
-    let tipsTime = 300;
-    let handler = 0;
-    let time = 200;
-    let chromeSpeedSpeed = 0.55;
-    let isEdge = navigator.userAgent.includes('Edg') ? true : false;
-
-    console.log('isEdge:', isEdge);
-
-    function toggleScroll() {
-        enable = !enable;
-        if (enable) {
-            if (isEdge) {
-                edgeAutoScroll();
-            } else {
-                autoScroll();
+    function scrollAnimation(currentTime) {
+        if (isScrolling) {
+            const elapsedTime = currentTime - lastScrollTime;
+            if (elapsedTime >= targetFrameDelay) {
+                const deltaScroll = scrollSpeed * (elapsedTime / targetFrameDelay);
+                scrollY += deltaScroll;
+                window.scrollTo(0, scrollY);
+                lastScrollTime = currentTime;
             }
-            showNotification('AutoScroll:On', tipsTime);
+            animationFrameId = requestAnimationFrame(scrollAnimation);
         } else {
-            if (isEdge) {
-                cancelAnimationFrame(animationFrameId);
-            } else {
-                clearTimeout(handler);
-            }
-            showNotification('AutoScroll:Off', tipsTime);
-        };
+            cancelAnimationFrame(animationFrameId);
+            animationFrameId = null;
+        }
     }
 
-    function keydownHandler(e) {
-        console.log(e.key);
+    // 开始滚动
+    function startScroll() {
+        isScrolling = true;
+        scrollY = window.scrollY || document.documentElement.scrollTop;
+        lastScrollTime = performance.now();
+        animationFrameId = requestAnimationFrame(scrollAnimation);
+    }
+
+    // 停止滚动
+    function stopScroll() {
+        isScrolling = false;
+        if (animationFrameId !== null) {
+            cancelAnimationFrame(animationFrameId);
+            animationFrameId = null;
+        }
+        scrollY = 0; // 清空滚动位置
+        lastScrollTime = 0; // 重置上次滚动时间
+    }
+
+    let lastKeyDownTime = 0;
+    let keyDownCount = 0;
+    // 键盘事件处理
+    document.addEventListener('keydown', (event) => {
         let currentTime = new Date().getTime();
-        switch (e.key) {
+        switch (event.key) {
+            case 's':
+            case 'ArrowLeft':
+                let step = 0.01
+                if (scrollSpeed > 2) {
+                    step = 0.1
+                }
+                scrollSpeed = Math.max(scrollSpeed - step, 0.01);
+                showNotification('AutoScroll:Speed: ' + roundToTwoDecimalPlaces(scrollSpeed), tipsTime);
+                break;
+            case 'a':
+            case 'ArrowRight':
+                scrollSpeed = Math.min(scrollSpeed + 0.1, 99);
+                showNotification('AutoScroll:Speed: ' + roundToTwoDecimalPlaces(scrollSpeed), tipsTime);
+                break;
             case "`":
                 if (currentTime - lastKeyDownTime < 300) {
                     keyDownCount++;
@@ -121,57 +114,16 @@
                 lastKeyDownTime = currentTime;
 
                 if (keyDownCount === 2) {
-                    toggleScroll();
+                    if (isScrolling) {
+                        stopScroll();
+                        showNotification('AutoScroll:Off', tipsTime);
+                    } else {
+                        startScroll();
+                        showNotification('AutoScroll:On', tipsTime);
+                    }
                     keyDownCount = 0;
                 }
                 break;
-            case "a":
-            case "ArrowRight":
-                if (isEdge && enable) {
-                    scrollSpeed = roundToTwoDecimalPlaces(scrollSpeed += 0.02);
-                    showNotification('Speed:' + scrollSpeed, tipsTime);
-                }
-                if (!isEdge && enable) {
-                    time -= 20;
-                    chromeSpeedSpeed = roundToTwoDecimalPlaces(chromeSpeedSpeed += 0.02);
-                    showNotification('Speed:' + chromeSpeedSpeed, tipsTime);
-                }
-                break;
-            case "s":
-            case "ArrowLeft":
-                if (isEdge && enable) {
-                    if (scrollSpeed - 0.02 > 0) {
-                        scrollSpeed -= 0.02
-                    }
-                    scrollSpeed = roundToTwoDecimalPlaces(scrollSpeed);
-                    showNotification('Speed:' + scrollSpeed, tipsTime);
-                }
-                if (!isEdge && enable) {
-                    time += 20;
-                    if (chromeSpeedSpeed - 0.02 <= 0) {
-                        chromeSpeedSpeed = 0.02
-                    }else{
-                        chromeSpeedSpeed = roundToTwoDecimalPlaces(chromeSpeedSpeed -= 0.02);
-                    }
-                    showNotification('Speed:' + chromeSpeedSpeed, tipsTime);
-                }
-                break;
         }
-    }
-    function edgeAutoScroll() {
-        console.log("edgeAutoScroll: " + document.documentElement.scrollTop);
-        console.log(window.scrollY, window.pageYOffset, document.documentElement.scrollTop);
-        document.documentElement.scrollTop += scrollSpeed;
-        animationFrameId = requestAnimationFrame(edgeAutoScroll);
-    }
-    function autoScroll() {
-        console.log("autoScroll: " + document.documentElement.scrollTop);
-        console.log(window.scrollY, window.pageYOffset, document.documentElement.scrollTop);
-        document.documentElement.scrollTop += 1 / window.devicePixelRatio + 0.05;
-        handler = setTimeout(autoScroll, time);
-    }
-
-    // 监听键盘事件
-    document.body.removeEventListener('keydown', keydownHandler);
-    document.body.addEventListener('keydown', keydownHandler);
+    });
 })(document);
--- AutoScroll2/README.md
+++ AutoScroll2/README.md
@@ -6,5 +6,18 @@
 
 #### 1.[安装脚本](https://greasyfork.org/zh-CN/scripts/401596-autoscroll2)<br/>
 #### 2.打开需要自动滚动的页面<br/>
-#### 3.双击两次 "`"(Tab键上面的)  滚动/停止<br/>
-#### 4.可以使用方向键 ->增加滚动速度、<-降低滚动速度<br/>
+#### 3.快速双击两次 "`"(Tab键上面的)  滚动/停止<br/>
+#### 4.可以使用方向键 `->` 或者 `a` 增加滚动速度、`<-` 或者 `s` 降低滚动速度<br/>
+
+
+
+# Auto Scroll page, support shortcut keys (Auto Scroll)
+
+## Auto Scroll Page
+
+### Thanks to the source script [AutoScroll](https://greasyfork.org/scripts/17851-autoscroll/code/AutoScroll.user.js)
+
+#### 1.[Install the script] (https://greasyfork.org/zh-CN/scripts/401596-autoscroll2)
+#### 2.Open the page that needs to be auto-scrolled.
+#### 3.Quick double click twice "`" (Tab key above)
+#### 4.You can use the arrow keys `->` or `a` to increase the scroll speed, `<-` or `s` to reduce the scroll speed.
